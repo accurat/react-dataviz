d0d540c714e24b3b6a1a07f99bb116a6046f2a5c
cesare.soldini <cesare.soldini@accurat.it>
2017-10-08 16:22:04 +0200

    📈 Improve test-app visualization

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -6,9 +6,6 @@ import { Viz, SubViz, Polyline, Squares, Circles, buildReactiveMouse } from 'rea
 const data1 = [ 0.12, 0.65, 0.76, 0.73, 0.64, 0.76, 0.22, 0.32, 0.83, 0.18, 0.27 ]
 const data2 = [ 0.15, 0.12, 0.22, 0.76, 0.73, 1.00, 0.64, 0.76, 0.32, 0.28, 0.29 ]
 
-const style1 = { stroke: 'blue', fill: 'rgba(0, 0, 255, 0.1)', opacity: 0.5 }
-const style2 = { stroke: 'red', fill: 'none', opacity: 0.5 }
-
 function dataToPoints(data) {
   const interval = 1 / (data.length - 1)
   return data.map((d, i) => ({ x: interval * i, y: d }))
@@ -26,7 +23,7 @@ class MouseDisplay extends React.Component {
           {mouseAttrs.map(([k, v]) =>
             <tr key={k}>
               <td className="w2 blue"> {k} </td>
-              <td className="w4"> {displayFloatOrValue(v)} </td>
+              <td className="w4 gray"> {displayFloatOrValue(v)} </td>
             </tr>
           )}
         </tbody>
@@ -47,16 +44,22 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="w-100 h-100 bg-black-10">
+      <div className="w-100 h-100 bg-black">
         <div className="absolute pa2 bottom-0 right-0 code">
           <MouseDisplay mouse={this.mouse} />
         </div>
-        <Viz margin={{ vert: 50, horiz: 50 }} mouse={this.mouse} debug="green">
-          <SubViz flipY from={[0, 0]} to={[1, 1]} margin={20} debug="steelblue">
-            <Polyline points={dataToPoints(data1)} style={style1} closed />
-            <Circles points={dataToPoints(data1)} style={{ stroke: 'none', fill: 'steelblue', opacity: 0.5 }} />
-            <Polyline points={dataToPoints(data2)} style={style2} />
-            <Squares points={dataToPoints(data2)} style={{ stroke: 'none', fill: 'yellow', opacity: 0.5 }} />
+        <Viz margin={{ vert: 50, horiz: 50 }} mouse={this.mouse}>
+          <SubViz flipY from={[0, 0]} to={[0.5, 0.5]} margin={20} debug="#333">
+            <Polyline closed points={dataToPoints(data1)} style={{ stroke: 'none', fill: 'rgba(0, 0, 255, 0.5)' }} />
+          </SubViz>
+          <SubViz flipY from={[0.5, 0]} to={[1, 0.5]} margin={20} debug="#333">
+            <Polyline closed points={dataToPoints(data2)} style={{ stroke: 'none', fill: 'rgba(255, 0, 0, 0.5)' }} />
+          </SubViz>
+          <SubViz flipY from={[0, 0.5]} to={[1, 1]} margin={20} debug="#333">
+            <Circles points={dataToPoints(data1)} style={{ fill: 'green' }} />
+            <Polyline points={dataToPoints(data1)} style={{ stroke: 'green' }} />
+            <Squares points={dataToPoints(data2)} style={{ fill: 'purple' }} />
+            <Polyline points={dataToPoints(data2)} style={{ stroke: 'purple' }} />
           </SubViz>
         </Viz>
       </div>
